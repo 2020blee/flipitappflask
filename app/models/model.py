21bc3672ce5bc294d2680398123fdbd3a0bce5eb
2020blee <2020blee@dwight.edu>
2019-09-17 14:39:47 -0400

    First commit

---
+++ app/models/model.py
@@ -0,0 +1,10 @@
+def flipit(str):
+
+    newstring = ""
+    for n in str:
+        newstring = n + newstring
+    return newstring
+
+def shout(str):
+
+    return str.upper()
---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from app import app
+from flask import render_template, request
+from app.models import model, formopener
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/results', methods = ["GET", "POST"])
+def results():
+    userdata = dict(request.form)
+    print(userdata)
+    nickname = userdata['nickname']
+    output = model.flipit(nickname)
+    return render_template("results.html", output = output)
